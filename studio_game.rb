06ee958f9a9c7e873827005a04896a01ac514ad4
Michael Wiss <michael.wiss@gmail.com>
2014-12-12 13:07:07 -0600

    numbers + strings

--- studio_game.rb
+++ studio_game.rb
@@ -1,5 +1,33 @@
-greeting = "Welcome!"
- 3.times do
-  puts greeting.upcase 
-end
-puts Time.now
+#greeting = "Welcome!"
+# 3.times do
+#  puts greeting.upcase 
+#end
+#puts Time.new
+
+
+#string interpolation: #{greeting}
+
+name1 = "larry"
+hvalue = 60
+
+puts name1 + '\'s' + ' health is ' + hvalue.to_s
+
+puts "#{name1}'s + health is  + #{hvalue * 3}"
+
+puts "#{name1}'s + health is  + #{hvalue / 9.to_f}"
+
+puts "#{name1}'s + health is  + #{hvalue / 9.to_i}"
+
+puts "#{name1}'s + health is  + #{hvalue / 9.0}"
+
+puts "#{name1}'s + health is  + #{hvalue / 9}"
+
+name2 = "curly"
+name3 = "moe"
+
+puts "Players: \n \t #{name1} \n \t #{name2} \n \t #{name3} "
+
+
+
+
+
