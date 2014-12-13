edf9397feeb7f6e18b153b1696da61076338295f
Michael Wiss <michael.wiss@gmail.com>
2014-12-13 15:57:55 -0600

    studio game and crowdfund updated

---
+++ crowdfund.rb
@@ -0,0 +1,8 @@
+project1 = "ProjectA"
+project2 = "ProjectB"
+project3 = "ProjectC"
+
+p1value = "$1000"
+
+puts "#{project1} has #{p1value} in funding"
+puts "Projects: \t\n #{project1} \t\n #{project2} \t\n #{project3}"
--- studio_game.rb
+++ studio_game.rb
@@ -7,26 +7,34 @@
 
 #string interpolation: #{greeting}
 
-name1 = "larry"
-hvalue = 60
+#name1 = "larry"
+#hvalue = 60
 
-puts name1 + '\'s' + ' health is ' + hvalue.to_s
+#puts name1 + '\'s' + ' health is ' + hvalue.to_s
 
-puts "#{name1}'s + health is  + #{hvalue * 3}"
+# puts "#{name1}'s + health is  + #{hvalue * 3}"
 
-puts "#{name1}'s + health is  + #{hvalue / 9.to_f}"
+# puts "#{name1}'s + health is  + #{hvalue / 9.to_f}"
 
-puts "#{name1}'s + health is  + #{hvalue / 9.to_i}"
+# puts "#{name1}'s + health is  + #{hvalue / 9.to_i}"
 
-puts "#{name1}'s + health is  + #{hvalue / 9.0}"
+# puts "#{name1}'s + health is  + #{hvalue / 9.0}"
 
-puts "#{name1}'s + health is  + #{hvalue / 9}"
+# puts "#{name1}'s + health is  + #{hvalue / 9}"
 
-name2 = "curly"
-name3 = "moe"
+# name2 = "curly"
+# name3 = "moe"
 
-puts "Players: \n \t #{name1} \n \t #{name2} \n \t #{name3} "
+# puts "Players: \n \t #{name1} \n \t #{name2} \n \t #{name3} "
 
+project1 = "ProjectA"
+project2 = "ProjectB"
+project3 = "ProjectC"
+
+p1value = "$1000"
+
+puts "#{project1} has #{p1value} in funding"
+puts "Projects: \t\n #{project1} \t\n #{project2} \t\n #{project3}"
 
 
 
