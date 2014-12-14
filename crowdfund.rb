64c3909cd3943caf0b0d2fdaf40633db485fd188
Michael Wiss <michael.wiss@gmail.com>
2014-12-14 10:40:23 -0600

    crowdfund - variables and objects

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,8 +1,23 @@
-project1 = "ProjectA"
-project2 = "ProjectB"
-project3 = "ProjectC"
+project1 = "projectA"
+project2 = "projectB"
+project3 = "projectC"
+project4 = "projectD"
+
 
 p1value = "$1000"
+p2value = "$2000"
+p3value = "$3000"
+p4value = "$4000"
 
-puts "#{project1} has #{p1value} in funding"
-puts "Projects: \t\n #{project1} \t\n #{project2} \t\n #{project3}"
+
+# puts "#{project1} has #{p1value} in funding"
+# puts "Projects: \t\n #{project1} \t\n #{project2} \t\n #{project3}"
+
+puts "Project totals:"
+
+    puts "\t\n #{project1.capitalize.ljust(30,'.')} #{p1value} total"
+    puts "\t\n #{project2.capitalize.ljust(30,'.')} #{p2value} total"
+    puts "\t\n #{project3.capitalize.ljust(30,'.')} #{p3value} total"
+    puts "\t\n #{project4.capitalize.ljust(30,'.')} #{p4value} total"
+
+    
