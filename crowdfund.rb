6d4119c8cfe77d2ab7dbe000f31ad4c22d80f953
Michael Wiss <michael.wiss@gmail.com>
2014-12-15 10:49:29 -0600

    crowdfund method

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,23 +1,31 @@
-project1 = "projectA"
-project2 = "projectB"
-project3 = "projectC"
-project4 = "projectD"
+# project1 = "projectA"
+# project2 = "projectB"
+# project3 = "projectC"
+# project4 = "projectD"
 
 
-p1value = "$1000"
-p2value = "$2000"
-p3value = "$3000"
-p4value = "$4000"
+# p1value = "$1000"
+# p2value = "$2000"
+# p3value = "$3000"
+# p4value = "$4000"
 
 
 # puts "#{project1} has #{p1value} in funding"
 # puts "Projects: \t\n #{project1} \t\n #{project2} \t\n #{project3}"
 
-puts "Project totals:"
+# puts "Project totals:"
 
-    puts "\t\n #{project1.capitalize.ljust(30,'.')} #{p1value} total"
-    puts "\t\n #{project2.capitalize.ljust(30,'.')} #{p2value} total"
-    puts "\t\n #{project3.capitalize.ljust(30,'.')} #{p3value} total"
-    puts "\t\n #{project4.capitalize.ljust(30,'.')} #{p4value} total"
+#     puts "\t\n #{project1.capitalize.ljust(30,'.')} #{p1value} total"
+#     puts "\t\n #{project2.capitalize.ljust(30,'.')} #{p2value} total"
+#     puts "\t\n #{project3.capitalize.ljust(30,'.')} #{p3value} total"
+#     puts "\t\n #{project4.capitalize.ljust(30,'.')} #{p4value} total"
 
-    
+
+#Methods
+
+def project_total(project, total=0)
+	"#{project.capitalize} has a funding amount of #{total}."
+end
+
+
+puts project_total("project1", 1500)
