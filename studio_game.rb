94e598327f92355e9aedfffa82b4386068a20a04
Michael Wiss <michael.wiss@gmail.com>
2014-12-15 09:04:19 -0600

    Methods

--- studio_game.rb
+++ studio_game.rb
@@ -43,35 +43,84 @@
 # puts "Projects: \t\n #{project1} \t\n #{project2} \t\n #{project3}"
 
 
-name1 = "larry"
-health1 = 60
-puts "#{name1.capitalize} has a health of #{health1}."
+# name1 = "larry"
+# health1 = 60
+# puts "#{name1.capitalize} has a health of #{health1}."
 
-name2 ="curly"
-health2="125"
-puts "#{name2.upcase} has a health of #{health2}."
-health2 =health1
-puts "#{name2.upcase} has a health of #{health2}."
-health1 = 30
-puts "#{name1.capitalize} has a health of #{health1}."
-puts "#{name2.upcase} has a health of #{health2}."
+# name2 ="curly"
+# health2="125"
+# puts "#{name2.upcase} has a health of #{health2}."
+# health2 =health1
+# puts "#{name2.upcase} has a health of #{health2}."
+# health1 = 30
+# puts "#{name1.capitalize} has a health of #{health1}."
+# puts "#{name2.upcase} has a health of #{health2}."
 
-name3 ="moe"
-health3 = 100
-#puts "#{name3.capitalize.rjust(30,"*")} has a health of #{health3.ljust(30,"*")}."
-text = "#{name3.capitalize} has a health of #{health3}."
-puts text.center(100,"*")
+# name3 ="moe"
+# health3 = 100
+# #puts "#{name3.capitalize.rjust(30,"*")} has a health of #{health3.ljust(30,"*")}."
+# text = "#{name3.capitalize} has a health of #{health3}."
+# puts text.center(100,"*")
 
-name4 ="Shemp"
-health4 = 90
-#puts "#{name4}#{health4.ljust(100,'.')}health"
-#^not a string
-puts "#{name4.capitalize.ljust(30,'.')} #{health4} health."
+# name4 ="Shemp"
+# health4 = 90
+# #puts "#{name4}#{health4.ljust(100,'.')}health"
+# #^not a string
+# puts "#{name4.capitalize.ljust(30,'.')} #{health4} health."
 
-puts 123.to_s.reverse.to_i
+# puts 123.to_s.reverse.to_i
 
-current_time = Time.new
-puts current_time
+# current_time = Time.new
+# puts current_time
+
+
+#Methods
+
+def movie_listing(title)
+	 "Movie: #{title.capitalize}"
+end
+
+puts movie_listing("goonies")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
