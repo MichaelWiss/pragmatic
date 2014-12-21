8ad6ba28727354c55a905055ad73f748742bcdfa
Michael Wiss <michael.wiss@gmail.com>
2014-12-21 14:42:36 -0600

    seperated source files

---
+++ player.rb
@@ -0,0 +1,44 @@
+class Player
+	attr_accessor :name
+    attr_reader :health
+
+
+	def initialize(name, health=100)
+		@name = name.capitalize
+		@health = health
+    end
+
+    def blam
+    	@health -=10
+    	puts "#{@name} got blammed!"
+    end
+
+    def woot
+    	@health +=15
+    	puts "#{@name} got wooted!"
+    end
+
+    def score
+    	@health + @name.length
+    end
+
+    def name=(new_name)
+        @name = new_name.capitalize
+    end
+
+
+    def to_s
+    	" #{@name} has a health of #{@health} and a score of #{score}."
+    end
+end
+
+
+if __FILE__ == $0
+player = Player.new("moe")
+puts player.name
+puts player.health
+player.w00t
+puts player.health
+player.blam
+puts player.health
+end
--- studio_game.rb
+++ studio_game.rb
@@ -274,39 +274,7 @@
 # puts books
 
 
-class Player
-	attr_accessor :name
-    attr_reader :health
 
-
-	def initialize(name, health=100)
-		@name = name.capitalize
-		@health = health
-    end
-
-    def blam
-    	@health -=10
-    	puts "#{@name} got blammed!"
-    end
-
-    def woot
-    	@health +=15
-    	puts "#{@name} got wooted!"
-    end
-
-    def score
-    	@health + @name.length
-    end
-
-    def name=(new_name)
-        @name = new_name.capitalize
-    end
-
-
-    def to_s
-    	" #{@name} has a health of #{@health} and a score of #{score}."
-    end
-end
 
 # player1 = Player.new("moe")
 # player2 = Player.new("larry", 60)
@@ -343,33 +311,8 @@ end
 # 	puts player
 # end
 
-class Game
-	attr_reader :title
-
-	def initialize(title)
-	  @title = title
-	  @players = []
-    end
-    def add_player(a_player)
-      @players.push(a_player)
-    end
-
-    def play
-      puts "There are #{@players.size} players in #{@title}: "
-      puts @player
-      @players.each do |player|
-      	
-      end
-      @players.each do |player|
-      	player.blam
-      	player.woot
-      	player.woot
-      	player.blam
-      	
-      	puts player
-    end
-  end
-end
+require_relative 'player'
+require_relative 'game'
 
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
