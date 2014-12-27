34c9efd8f9e4ee55e0f33f427b351df69b36acaf
Michael Wiss <michael.wiss@gmail.com>
2014-12-27 16:19:43 -0600

    test

--- project_spec.rb
+++ project_spec.rb
@@ -1,1 +1,29 @@
 require_relative 'project'
+
+describe Project do
+
+  before do
+     @amount = 1000
+     @project = Project.new("project1", @amount)
+   end
+
+it "has a capitalized name" do
+      
+      expect(@project.name).to eq("Project1")
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
