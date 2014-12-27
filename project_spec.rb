e2faa0afc3e9fefc84a04a94d94ecdca068ab29f
Michael Wiss <michael.wiss@gmail.com>
2014-12-27 16:34:31 -0600

    rspec

--- project_spec.rb
+++ project_spec.rb
@@ -3,27 +3,23 @@ require_relative 'project'
 describe Project do
 
   before do
+  	 
      @amount = 1000
      @project = Project.new("project1", @amount)
    end
 
-it "has a capitalized name" do
+ it "has a capitalized name" do
       
       expect(@project.name).to eq("Project1")
+   end
+
+ it "has an amount" do  
+      expect(@amount).to eq(1000)
   end
 
+  it "increases health by 500 when gaining funds" do
+     @project.gain_funds
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+      expect(@project.amount).to eq(@amount + 500)
+   end
 end
