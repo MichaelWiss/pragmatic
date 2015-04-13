3a9f62b28ba9a5dd2950d62111fc863c9e451f89
Michael Wiss <michael.wiss@gmail.com>
2015-04-13 17:01:13 -0500

    review

--- pragmatic_review.rb
+++ pragmatic_review.rb
@@ -90,6 +90,43 @@ players.pop
 player4 = Player.new("Shemp", 90)
 players.push(player4)
 
+class Game
+	attr_reader :title
+
+	def initialize(title)
+		@title = title
+		@players = []
+	end
+
+	def add_player(a_player)
+		@players.push(a_player)
+    end
+
+    def play
+      puts "There are #{@players.size} players in #{@title}: "
+      @players.each do |player|
+       puts player
+    end
+      @players.each do |player|
+      player.blam
+      player.w00t
+      player.w00t
+      puts player
+    end
+  end
+
+end
+
+player1 = Player.new("moe")
+player2 = Player.new("larry", 60)
+player3 = Player.new("curly", 125)
+    
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
+
 
 
 
