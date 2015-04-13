1a0b2ee5e9a067788cd2128dcb8b7348cde120a9
Michael Wiss <michael.wiss@gmail.com>
2015-04-13 15:37:28 -0500

    review

--- pragmatic_review.rb
+++ pragmatic_review.rb
@@ -18,7 +18,7 @@ class Player
 	attr_reader :health
 	attr_accessor :name
 	def initialize(name, health=100)
-		@name = name
+		@name = name.capitalize
 		@health = health
 	end
 
@@ -38,20 +38,59 @@ class Player
     	"I'm #{@name} with a health of #{@health} and a score of #{score}."
     end
 
+    def blam
+      @health -= 10
+      puts "#{@name} got blammed!"
+    end
+  
+    def w00t
+      @health += 15
+      puts "#{@name} got w00ted!"
+    end
+
    
 end
 
 
+
+
 player1 = Player.new("moe", 100)
 player2 = Player.new("larry", 100)
-puts player1.say_hello
-puts player2.say_hello
-puts player2.health
+# puts player1.say_hello
+# puts player2.say_hello
+# puts player2.health
 player2.name = "lawrence"
 player1.name = "moe"
-puts player2.name
-puts player2.to_s
-puts player1.to_s
-puts player1.name
+# puts player2.name
+# puts player2.to_s
+# puts player1.to_s
+# puts player1.name
+player3 = Player.new("curly", 60)
+
+players = [player1, player2, player3]
+
+puts "There are #{players.size} players in the game:"
+
+players.each do |player|
+  puts player
+end
+
+players.each do |player|
+  puts player.health
+end
+
+players.each do |player|
+  player.blam
+  player.w00t
+  player.w00t
+  puts player
+end
+
+players.pop
+player4 = Player.new("Shemp", 90)
+players.push(player4)
+
+
+
 
 
