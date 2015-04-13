3ce4d78938519a14cee6e54bd6e1344b7393d76b
Michael Wiss <michael.wiss@gmail.com>
2015-04-13 12:25:17 -0500

    recap

--- pragmatic_review.rb
+++ pragmatic_review.rb
@@ -15,6 +15,8 @@
 # puts say_hello("moe", 200)
 
 class Player
+	attr_reader :health
+	attr_accessor :name
 	def initialize(name, health=100)
 		@name = name
 		@health = health
@@ -23,6 +25,20 @@ class Player
 	def say_hello
 		"I'm #{@name} with a health of #{@health}."
     end
+
+    def score
+    	@health + @name.length
+    end
+
+    def name=(new_name)
+    	@name = new_name.capitalize
+    end
+
+    def to_s
+    	"I'm #{@name} with a health of #{@health} and a score of #{score}."
+    end
+
+   
 end
 
 
@@ -30,4 +46,12 @@ player1 = Player.new("moe", 100)
 player2 = Player.new("larry", 100)
 puts player1.say_hello
 puts player2.say_hello
+puts player2.health
+player2.name = "lawrence"
+player1.name = "moe"
+puts player2.name
+puts player2.to_s
+puts player1.to_s
+puts player1.name
+
 
