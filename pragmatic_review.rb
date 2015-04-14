a4770134e54650d7e163b5d9ea6e44fba840bb3e
Michael Wiss <michael.wiss@gmail.com>
2015-04-14 16:18:15 -0500

    review

--- pragmatic_review.rb
+++ pragmatic_review.rb
@@ -61,11 +61,13 @@ require_relative 'game_review'
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
+player4 = Player.new("dickie", 125)
     
 knuckleheads = Game.new("Knuckleheads")
 knuckleheads.add_player(player1)
 knuckleheads.add_player(player2)
 knuckleheads.add_player(player3)
+knuckleheads.add_player(player4)
 knuckleheads.play
 
 
